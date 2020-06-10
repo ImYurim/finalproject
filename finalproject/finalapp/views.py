a990aa7dd30f58ce7520a5145f22adb029593f30
yurim <ymyu124@gmail.com>
2020-06-10 20:03:31 +0900

    유사점

--- finalproject/finalapp/views.py
+++ finalproject/finalapp/views.py
@@ -19,7 +19,19 @@ def competitorresult(request):
         companydetail = Company.objects.all().filter(
             name=company)
         if companydetail:
-            return render(request, 'finalapp/competitorresult.html', {'companydetail': companydetail, 'company': company})
+            mycompany = Company.objects.get(name=company)
+            competitor1_name = mycompany.competitor1
+            competitor1 = Company.objects.get(name=competitor1_name)
+            competitor1_strength = [competitor1.strength1, competitor1.strength2,
+                                    competitor1.strength3, competitor1.strength4, competitor1.strength5]
+            mycompany_strength = [mycompany.strength1, mycompany.strength2,
+                                  mycompany.strength3, mycompany.strength4, mycompany.strength5]
+            similar1 = []
+            for s in mycompany_strength:
+                if s in competitor1_strength:
+                    similar1.append(s)
+
+            return render(request, 'finalapp/competitorresult.html', {'companydetail': companydetail, 'company': company, 'similar1': similar1})
         else:
             companyall = Company.objects.all()
             return render(request, 'finalapp/competitor.html', {'error': True, 'companyall': companyall})
